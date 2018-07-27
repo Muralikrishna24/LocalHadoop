bd5bbec3779bf73391fbd959a2de9a3e7bed8b3a
Murali <murali.rnec@gmail.com>
2018-07-27 15:19:29 +0530

    making proper changes.

--- LocalHadoop/src/main/java/WelcomeGit.java
+++ LocalHadoop/src/main/java/WelcomeGit.java
@@ -1,8 +1,10 @@
 
 public class WelcomeGit {
 	public static void main(String[] args) {
+		Git1 git1 = new Git1();
 		System.out.println("WELCOME TO GIT:Test1");
 		System.out.println("Merge Test");
+		git1.display();
 	}
 
 }
